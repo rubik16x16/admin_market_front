686f8db5da80ea0294d4d479d3d314cd9644e385
anthony.gonzalez <anthony.gonzalez@alphas.technology>
2024-05-06 23:25:03 +0000

    chore(pages): create user index page

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
 import { routes } from './routes';
 
 @NgModule({
   imports: [
-    RouterModule.forChild(routes)
+    FlexLayoutModule,
+		RouterModule.forChild(routes),
+    MatSidenavModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		MatGridListModule,
+		MatCardModule
   ],
   exports: [
     RouterModule
   ],
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   providers: [],
 })
 
